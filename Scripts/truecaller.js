f8c3943b1dc671554e98aa8a49e1c4720a672b0b
blackcatxhan <blackcatxhan@users.noreply.github.com>
2023-05-01 12:10:55 +0700

    Update truecaller.js

--- Scripts/truecaller.js
+++ Scripts/truecaller.js
@@ -15,6 +15,24 @@ const features = [
   { id: "premium_support", rank: 11, status: "Included" },
   { id: "gold_caller_id", rank: 13, status: "Included" }
 ];
+const features_sub = [
+  { id: "spam_blocking", rank: -2147483648, status: "Included", isFree: true },
+  { id: "caller_id", rank: -2147483648, status: "Included", isFree: true },
+  { id: "siri_search", rank: 1, status: "Included", isFree: false },
+  { id: "no_ads", rank: 2, status: "Included", isFree: false },
+  { id: "extended_spam_blocking", rank: 3, status: "Included", isFree: false },
+  { id: "call_assistant", rank: 4, status: "Excluded", isFree: false },
+  { id: "assistant_voicemail", rank: 5, status: "Excluded", isFree: false },
+  { id: "assistant_custom_greeting", rank: 6, status: "Excluded", isFree: false },
+  { id: "ct_call_recording", rank: 7, status: "Excluded", isFree: false },
+  { id: "who_viewed_my_profile", rank: 9, status: "Included", isFree: false },
+  { id: "incognito_mode", rank: 11, status: "Included", isFree: false },
+  { id: "premium_badge", rank: 15, status: "Included", isFree: false },
+  { id: "premium_support", rank: 16, status: "Included", isFree: false },
+  { id: "live_chat_support", rank: 17, status: "Included", isFree: false },
+  { id: "family_sharing", rank: 18, status: "Excluded", isFree: false },
+  { id: "gold_caller_id", rank: 19, status: "Included", isFree: false }
+];
 var obj;
 switch ($request.url) {
   case findUrl(/subscriptions\/status/):
@@ -33,7 +51,7 @@ switch ($request.url) {
           productType: "GoldYearly",
           isFreeTrial: true
         },
-        tier: { id: "gold", feature: features }
+        tier: { id: "gold", feature: features_sub }
       }
     break;
   case findUrl(/products\/apple/):
