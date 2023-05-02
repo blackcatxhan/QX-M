1c47fb8d35747ac1ff1d30a83b2c1b6352c1a6a8
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-05-02 07:53:58 +0700

    Update ForTest.js

--- Scripts/Checking/ForTest.js
+++ Scripts/Checking/ForTest.js
@@ -1,26 +1,4 @@
-var obj = JSON.parse($response.body); 
-obj['subscriber']['subscriptions'] = {
-      "remotetv.weekly.02" : {
-        "original_purchase_date" : "2023-05-01T11:27:02Z",
-        "expires_date" : "2099-07-07T11:27:01Z",
-        "is_sandbox" : false,
-        "refunded_at" : null,
-        "unsubscribe_detected_at" : "2023-05-01T11:27:28Z",
-        "grace_period_expires_date" : null,
-        "period_type" : "trial",
-        "purchase_date" : "2023-05-01T11:27:01Z",
-        "billing_issues_detected_at" : null,
-        "ownership_type" : "PURCHASED",
-        "store" : "app_store",
-        "auto_resume_date" : null
-      }
-    }; 
-obj['subscriber']['entitlements'] = {
-      "premium" : {
-        "grace_period_expires_date" : null,
-        "purchase_date" : "2023-05-01T11:27:01Z",
-        "product_identifier" : "blocker.ios.subscription.yearly",
-        "expires_date" : "2099-07-07T11:27:01Z"
-      }
-    }; 
+let obj = JSON.parse($response.body);
+obj.data.coins= 9999;
+obj.data.diamonds= 9999;
 $done({body: JSON.stringify(obj)});
