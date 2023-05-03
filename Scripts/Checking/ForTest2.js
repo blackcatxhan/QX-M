5e60d44126b01a4f813e14d5836a687c5fa2d32c
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-05-04 05:55:03 +0700

    Update ForTest2.js

--- Scripts/Checking/ForTest2.js
+++ Scripts/Checking/ForTest2.js
@@ -1,3 +1,14 @@
 {
-	"code" : 0
+  "result" : {
+    "products" : [
+      {
+        "managed" : false,
+        "status" : "ACTIVE",
+        "startDate" : 1683153619000,
+        "productId" : "com.snowcorp.epik.subscribe.plan.oneyear",
+        "expireDate" : 4087122786000
+      }
+    ],
+    "activated" : true
+  }
 }
