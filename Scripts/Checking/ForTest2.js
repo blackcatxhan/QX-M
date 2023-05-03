6fabbe2a8396425ca7ef20dd662101fc9f1911ff
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-05-04 06:32:47 +0700

    Update ForTest2.js

--- Scripts/Checking/ForTest2.js
+++ Scripts/Checking/ForTest2.js
@@ -1,11 +1,8 @@
 let obj = JSON.parse($response.body);
-obj["result"]["products"][]={
-  "managed" : false,
-        "status" : "ACTIVE",
-        "startDate" : 1683153619000,
-        "productId" : "com.snowcorp.epik.subscribe.plan.oneyear",
-        "expireDate" : 4087122786000
-      };
-obj["result"]["activated"]=true;
-      
+obj.result.products.managed = false;
+obj.result.products.status = "ACTIVE";
+obj.result.products.productId = "com.snowcorp.epik.subscribe.plan.oneyear";
+obj.result.products.startDate = 1683153619000;
+obj.result.products.expireDate = 4087122786000;
+obj.result.activated = true;
 $done({body: JSON.stringify(obj)});
