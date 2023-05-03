e726ab476ed86cdf3ff6452aae698388a8d961bc
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-05-04 06:11:28 +0700

    Update ForTest2.js

--- Scripts/Checking/ForTest2.js
+++ Scripts/Checking/ForTest2.js
@@ -1,14 +1,11 @@
-{
-  "result" : {
-    "products" : [
-      {
-        "managed" : false,
+let obj = JSON.parse($response.body);
+obj["result"]["products"]={
+  "managed" : false,
         "status" : "ACTIVE",
         "startDate" : 1683153619000,
         "productId" : "com.snowcorp.epik.subscribe.plan.oneyear",
         "expireDate" : 4087122786000
-      }
-    ],
-    "activated" : true
-  }
-}
+      };
+obj["result"]["activated"]=true;
+      
+$done({body: JSON.stringify(obj)});
