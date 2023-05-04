02068be57a03266d52b7a437611fbfcc4636615d
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-04 14:37:11 +0700

    Try update ID premium for Hanzii

--- Scripts/Checking/ForTest.js
+++ Scripts/Checking/ForTest.js
@@ -1,5 +1,6 @@
 var obj = JSON.parse($response.body); 
 obj['is_premium'] = 1; 
 obj['premium_expired'] = 1714720027; 
+obj['id'] = 293562; 
 obj['rating'] = false; 
 $done({body: JSON.stringify(obj)});
