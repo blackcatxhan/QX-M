33c6fd777b3d2a5ee6bed6d1d60b6fb4d9da23d1
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-05 14:22:46 +0700

    Update ForTest.js

--- Scripts/Checking/ForTest.js
+++ Scripts/Checking/ForTest.js
@@ -1,10 +1,5 @@
 var obj = JSON.parse($response.body);
 
-obj.data.plan_subscriptions[0].remaining_day = 7;
-obj.data.plan_subscriptions[0].promotion = "vip_reward";
-obj.data.plan_subscriptions[0].end_at = "07:07 07-07-2099";
-obj.data.plan_subscriptions[0].start_at = "07:07 05-05-2023"";
-obj.data.plan_subscriptions[0].name = "Gói VIP LK Team";
 obj.data.user_resource.can_playlist_coin = true;
 obj.data.user_resource.has_survey = false;
 obj.data.user_resource.remaining_coins = 7777777;
