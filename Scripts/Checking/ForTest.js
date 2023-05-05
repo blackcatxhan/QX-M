c415bfe88f0b43cb2a788c8d9e78d71e1eaa2d0a
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-05 11:16:14 +0700

    Final testing

--- Scripts/Checking/ForTest.js
+++ Scripts/Checking/ForTest.js
@@ -1,46 +1,18 @@
-var obj = JSON.parse($response.body); 
-delete obj.data.preview_time;
-// obj.data.preview_time = obj.data.total_time;
-if (obj.data.next_content.service_info.service === "Y") {
-	if (obj.data.next_content.service_info.flash_card !== "N") {
-		obj.data.next_content.user_service_info.flash_card = obj.data.next_content.service_info.flash_card;
-		obj.data.next_content.service_info.flash_card = "F";
-	}
-	if (obj.data.next_content.service_info.vocabulary !== "N") {
-		obj.data.next_content.user_service_info.vocabulary = obj.data.next_content.service_info.vocabulary;
-		obj.data.next_content.service_info.vocabulary = "F";
-	}
-	if (obj.data.next_content.service_info.ebook !== "N") {
-		obj.data.next_content.user_service_info.ebook = obj.data.next_content.service_info.ebook;
-		obj.data.next_content.service_info.ebook = "F";
-	}
-	if (obj.data.next_content.service_info.sentence !== "N") {
-		obj.data.next_content.user_service_info.sentence = obj.data.next_content.service_info.sentence;
-		obj.data.next_content.service_info.sentence = "F";
-	}
-	if (obj.data.next_content.service_info.quiz !== "N") {
-		obj.data.next_content.user_service_info.quiz = obj.data.next_content.service_info.quiz;
-		obj.data.next_content.service_info.quiz = "F";
-	}
-	if (obj.data.next_content.service_info.translate !== "N") {
-		obj.data.next_content.user_service_info.translate = obj.data.next_content.service_info.translate;
-		obj.data.next_content.service_info.translate = "F";
-	}
-	if (obj.data.next_content.service_info.story !== "N") {
-		obj.data.next_content.user_service_info.story = obj.data.next_content.service_info.story;
-		obj.data.next_content.service_info.story = "F";
-	}
-	if (obj.data.next_content.service_info.crossword !== "N") {
-		obj.data.next_content.user_service_info.crossword = obj.data.next_content.service_info.crossword;
-		obj.data.next_content.service_info.crossword = "F";
-	}
-	if (obj.data.next_content.service_info.original_text !== "N") {
-		obj.data.next_content.user_service_info.original_text = obj.data.next_content.service_info.original_text;
-		obj.data.next_content.service_info.original_text = "F";
-	}
-	if (obj.data.next_content.service_info.starwords !== "N") {
-		obj.data.next_content.user_service_info.starwords = obj.data.next_content.service_info.starwords;
-		obj.data.next_content.service_info.starwords = "F";
-	}
+function replaceYWithF(obj) {
+  for (var prop in obj) {
+    if (obj[prop] === 'Y') {
+      obj[prop] = 'F';
+    } else if (typeof obj[prop] === 'object') {
+      replaceYWithF(obj[prop]);
+    }
+  }
+}
+var obj = JSON.parse($response.body);
+if(url.indexOf('story')!=-1){
+	replaceYWithF(obj);
+}
+if(url.indexOf('player?')!=-1){
+	delete obj.data.preview_time;
+	replaceYWithF(obj);
 }
 $done({body: JSON.stringify(obj)});
--- Scripts/nhaccuatui.js
+++ Scripts/nhaccuatui.js
@@ -1,8 +1,8 @@
 let obj = JSON.parse($response.body);
-obj.data.vipExpire= "09.09.2099";
+obj.data.vipExpire= "07.07.2099";
 obj.data.isVIP= true;
-obj.data.displayName= "BlackCatX";
-obj.data.balanceNum= 9999;
-obj.data.balance= "9999 xu";
-obj.data.fullName= "BlackCatX";
+obj.data.displayName= "BlackCatX (LK Team)";
+obj.data.balanceNum= 7777;
+obj.data.balance= "7777 xu";
+obj.data.fullName= "BlackCatX (LK Team)";
 $done({body: JSON.stringify(obj)});
