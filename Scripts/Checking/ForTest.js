5a0bda726ee4b65c230226dfda68d5edda84c2a2
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-05 14:09:38 +0700

    Testing VOIZ unlock app

--- Scripts/Checking/ForTest.js
+++ Scripts/Checking/ForTest.js
@@ -1,19 +1,19 @@
-function replaceYWithF(obj) {
-  for (var prop in obj) {
-    if (obj[prop] === 'Y') {
-      obj[prop] = 'F';
-    } else if (typeof obj[prop] === 'object') {
-      replaceYWithF(obj[prop]);
-    }
-  }
-}
-let url=$request.url;
+const now = new Date();
+const futureDate = new Date("2099-07-07T07:07:00");
+const timeDiff = futureDate.getTime() - now.getTime();
+const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
 var obj = JSON.parse($response.body);
-if(url.indexOf('story')!=-1 || url.indexOf('song')!=-1){
-	replaceYWithF(obj);
-}
-if(url.indexOf('player?')!=-1){
-	delete obj.data.preview_time;
-	replaceYWithF(obj);
-}
+
+obj.data.plan_subscriptions[0].remaining_day = daysLeft;
+obj.data.plan_subscriptions[0].promotion = "v9_production_vip_12_month";
+obj.data.plan_subscriptions[0].end_at = "07:07 07-07-2099";
+obj.data.plan_subscriptions[0].start_at = "07:07 05-05-2023"";
+obj.data.plan_subscriptions[0].name = "Gói VIP LK Team";
+obj.data.user_resource.can_playlist_coin = true;
+obj.data.user_resource.has_survey = false;
+obj.data.user_resource.remaining_coins = 7777777;
+obj.data.user_resource.can_playlist_coin = true;
+obj.data.promotion = "v9_production_vip_12_month";
+
 $done({body: JSON.stringify(obj)});
