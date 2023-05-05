d9c3f320880327cc9ba01f18453d3ee8b40c45cf
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-05 10:04:28 +0700

    Fix unlock

--- Scripts/Checking/ForTest.js
+++ Scripts/Checking/ForTest.js
@@ -1,3 +1,13 @@
 var obj = JSON.parse($response.body); 
-obj.data.users[0].type = "S";
+obj.data.preview_time = obj.data.total_time;
+obj.data.next_content.user_service_info.flash_card = obj.data.next_content.service_info.flash_card;
+obj.data.next_content.user_service_info.vocabulary = obj.data.next_content.service_info.vocabulary;
+obj.data.next_content.user_service_info.ebook = obj.data.next_content.service_info.ebook;
+obj.data.next_content.user_service_info.sentence = obj.data.next_content.service_info.sentence;
+obj.data.next_content.user_service_info.quiz = obj.data.next_content.service_info.quiz;
+obj.data.next_content.user_service_info.translate = obj.data.next_content.service_info.translate;
+obj.data.next_content.user_service_info.story = obj.data.next_content.service_info.story;
+obj.data.next_content.user_service_info.crossword = obj.data.next_content.service_info.crossword;
+obj.data.next_content.user_service_info.original_text = obj.data.next_content.service_info.original_text;
+obj.data.next_content.user_service_info.starwords = obj.data.next_content.service_info.starwords;
 $done({body: JSON.stringify(obj)});
