15c36b700fae8ba1de476fca02304af88f26af03
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-05 09:06:39 +0700

    Update Chess.js

--- Scripts/Chess.js
+++ Scripts/Chess.js
@@ -1,10 +1,12 @@
 let obj = JSON.parse($response.body);
 let time = new Date();
+obj.data.is_apple_auto_renewable = false;
+obj.data.is_trial_eligible = true;
+obj.data.will_renew = true;
 obj.data.is_premium = 1;
 obj.data.type = "diamond";
 obj.data.sku = "diamond_yearly_032022";
 obj.data.is_apple_subscriber = true;
-obj.data.date.start = Math.floor(time / 1000);
-time.setFullYear(time.getFullYear() + 1);
-obj.data.date.expires = Math.floor(time / 1000);
+obj.data.date.start = time.setHours(time.getHours() - 1);
+obj.data.date.expires = time.setDate(time.getDate() + 7);
 $done({body: JSON.stringify(obj)});
