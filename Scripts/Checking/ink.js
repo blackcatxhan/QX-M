c3b657397209c3b639baafadd4f588428041dbee
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-09 12:56:19 +0700

    Fixed Panels and Ink Apps

--- Scripts/Checking/ink.js
+++ Scripts/Checking/ink.js
@@ -1,6 +1,12 @@
-let obj = JSON.parse($response.body);
-
-obj.data.attributes.subscriptions = {
+let obj = {
+  "data" : {
+    "type" : "adapty_analytics_profile",
+    "id" : "b2aa98ec-91ae-413c-904e-1576d015b8ee",
+    "attributes" : {
+      "app_id" : "c2e380b9-b8d5-4a2b-af78-8b9e50697fd3",
+      "total_revenue_usd" : 0,
+      "customer_user_id" : null,
+      "subscriptions" : {
         "digital.nestapps.ink.iap.annual.trial" : {
           "vendor_transaction_id" : "450001422813306",
           "billing_issue_detected_at" : null,
@@ -19,13 +25,17 @@ obj.data.attributes.subscriptions = {
           "is_refund" : false,
           "is_in_grace_period" : false,
           "active_introductory_offer_type" : "free_trial",
-          "expires_at" : "2099-07-07T07:27:43.000000+0000",
+          "expires_at" : "2099-07-07T02:27:43.000000+0000",
           "starts_at" : null,
           "is_sandbox" : false
         }
-      };
+      },
+      "promotional_offer_eligibility" : false,
+      "custom_attributes" : {
 
-obj.data.attributes.paid_access_levels = {
+      },
+      "profile_id" : "b2aa98ec-91ae-413c-904e-1576d015b8ee",
+      "paid_access_levels" : {
         "premium" : {
           "id" : "premium",
           "is_lifetime" : false,
@@ -34,7 +44,7 @@ obj.data.attributes.paid_access_levels = {
           "cancellation_reason" : null,
           "billing_issue_detected_at" : null,
           "unsubscribed_at" : "2023-05-08T02:28:13.915312+0000",
-          "expires_at" : "2099-07-07T07:27:43.000000+0000",
+          "expires_at" : "2099-07-07T02:27:43.000000+0000",
           "will_renew" : false,
           "is_active" : true,
           "active_promotional_offer_id" : null,
@@ -46,6 +56,11 @@ obj.data.attributes.paid_access_levels = {
           "store" : "app_store",
           "starts_at" : null
         }
-      };
+      },
+      "introductory_offer_eligibility" : false,
+      "non_subscriptions" : null
+    }
+  }
+};
 	
 $done({body: JSON.stringify(obj)});
