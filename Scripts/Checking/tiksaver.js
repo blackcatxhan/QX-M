43c89258a4afc66b7b5b9efa11015b61804006e8
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-09 11:05:47 +0700

    Update TikSaver

--- Scripts/Checking/tiksaver.js
+++ Scripts/Checking/tiksaver.js
@@ -1,6 +1,16 @@
-{
-	"code" : 8000,
-	"ok" : 0,
-	"codeName" : "AtlasError",
-	"name" : "MongoError"
+let url=$request.url;
+let obj=JSON.parse($response.body);
+if (url.endsWith('user')) {
+	obj = {
+		"code" : 8000,
+		"ok" : 0,
+		"codeName" : "AtlasError",
+		"name" : "MongoError"
+	};
+	$done({body:JSON.stringify(obj)});
+}
+
+if (url.endsWith('bookmark')) {
+	$response.writeHead(304);
+	$response.end();
 }
