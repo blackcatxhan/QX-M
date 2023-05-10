90a8c779f8c83d8e841ca6575f3c864107d7dcac
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-10 10:59:50 +0700

    Unlock Chapter in Eggbun app

--- Scripts/Checking/eggbun.js
+++ Scripts/Checking/eggbun.js
@@ -1,8 +1,23 @@
+function replaceLockedWithFalse(obj) {
+  for (var prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      replaceLockedWithFalse(obj[prop]);
+    } else if (prop === 'locked') {
+      obj[prop] = false;
+    }
+  }
+}
 let obj=JSON.parse($response.body);
-obj.isLifetimePremium = true;
-obj.isAdmin = true;
-obj.isGptChat = true;
-obj.expired = false;
-obj.isAlumni = true;
-obj.isSchoolPremium = true;
+if (url.indexOf('v3/accounts/sign-in') !== -1) {
+	obj.isLifetimePremium = true;
+	obj.isAdmin = true;
+	obj.isGptChat = true;
+	obj.expired = false;
+	obj.isAlumni = true;
+	obj.isSchoolPremium = true;
+}
+
+if (url.indexOf('v1/chapters/progress/enumerate') !== -1 && $response.status === 200) {
+	replaceLockedWithFalse(obj);
+}
 $done({body:JSON.stringify(obj)});
