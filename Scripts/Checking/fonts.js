27a90a5afd5d2d814fa481acadd2c6828f85cef9
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-10 13:51:44 +0700

    Unlocked Fonts and Edjing Mix apps. Removed Altstore.

---
+++ Scripts/Checking/fonts.js
@@ -0,0 +1,37 @@
+let obj = JSON.parse($response.body);
+let unique_id = obj.me.unique_id;
+
+obj.me = {
+    "non_consumables_ids" : [
+
+    ],
+    "active_subscriptions_ids" : [
+      "fontskeyboard.font.1y_t70_1w"
+    ],
+    "privacy_notice" : {
+      "required_consents" : [
+
+      ],
+      "is_at_least_16" : "unknown",
+      "last_acknowledged_version" : "2.1.0"
+    },
+    "unique_id" : unique_id,
+    "active_bundle_subscriptions" : [
+      {
+        "expiry" : "2023-07-07T07:07:07+00:00",
+        "product_id" : "fontskeyboard.font.1y_t70_1w",
+        "features" : [
+          "unlock"
+        ]
+      }
+    ],
+    "is_spooner" : false,
+    "terms_of_service" : {
+      "last_accepted_version" : "2.4.0"
+    },
+    "available_consumable_credits" : {
+
+    }
+  };
+
+$done({body: JSON.stringify(obj)});
