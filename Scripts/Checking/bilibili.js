b67b1f4b7f54ec83d75e29ab7f4931367b839fa5
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-11 17:21:17 +0700

    Update bilibili.js

--- Scripts/Checking/bilibili.js
+++ Scripts/Checking/bilibili.js
@@ -36,5 +36,27 @@ if (url.indexOf('app/account/myinfo') !== -1) {
 	  }
 	};
 }
+
+if (url.indexOf('app/account/mine') !== -1) {
+	obj.data.user_info.is_vip = true;
+	delete  obj.data.premium;
+	obj.data.user_info.badge = {
+		"name": "Premium",
+		"color": "#F26183",
+		"uri": "bstar://pgc/vip_zone?page_id=360009"
+	  };
+	obj.data.user_info.bg_accessory = {
+	  "show_guide": true,
+	  "landscape_img": "https://pic.bstarstatic.com/management/6b683b14c768b3ccccda6c93daf6ea90.png",
+	  "portrait_img": "https://pic.bstarstatic.com/management/b46a1310684e48948e52bc3b9cbb7349.png",
+	  "item_id": 659,
+	  "parent_item_id": 660,
+	  "suit_name": "Ảnh Bìa Premium Độc Quyền",
+	  "url": "https://www.bilibili.tv/mall-accessory/home?msource=21",
+	  "show_conf": {
+		"status_bar_mode": 1
+	  }
+	};
+}
 	
 $done({body: JSON.stringify(obj)});
