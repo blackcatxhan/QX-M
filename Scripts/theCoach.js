a776ff0fba273e9606b0febf7ff37406934737f5
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-16 14:08:43 +0700

    Update theCoach.js

--- Scripts/theCoach.js
+++ Scripts/theCoach.js
@@ -64,13 +64,13 @@ if (url.indexOf('v1/gym/course/listening/lesson?') !== -1) {
 	replaceValueToAny(obj, 'is_lock_login', false);
 }
 
-if (url.indexOf('v1/product-user') !== -1) {
+// if (url.indexOf('v1/product-user') !== -1) {
 	// obj.data.is_trial = true;
 	// obj.data.is_trial_x_day = true;
-	obj.data.day = 77777;
-	obj.data.hour = 7;
-	obj.data.second = 7;
-	obj.data.minute = 7;
-}
+	// obj.data.day = 77777;
+	// obj.data.hour = 7;
+	// obj.data.second = 7;
+	// obj.data.minute = 7;
+// }
 
 $done({body: JSON.stringify(obj)});
