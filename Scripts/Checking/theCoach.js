cdcf84f9ed8a4ea5f7742dc8bbbcd724a1218a4e
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-16 10:29:26 +0700

    Update theCoach.js

--- Scripts/Checking/theCoach.js
+++ Scripts/Checking/theCoach.js
@@ -42,6 +42,8 @@ if (url.indexOf('v1/communicate/topics') !== -1) {
 
 if (url.indexOf('v1/communicate/lessons') !== -1) {
 	replaceValueToAny(obj, 'is_trial', true);
+	replaceValueToAny(obj, 'access_type', 'USER');
+	// replaceValueToAny(obj, 'status', 'OPEN');
 }
 
 if (url.indexOf('v1/ai_conversation/topics?') !== -1) {
