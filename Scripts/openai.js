c6bb16556d92c0baa58ccda20bdccea846f3e72f
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-05-19 23:58:23 +0700

    Update openai.js

--- Scripts/openai.js
+++ Scripts/openai.js
@@ -160,13 +160,9 @@ $done({
         break;
     case findUrl(/conversation/):
        obj = $response.body;
-var result;
-
-if (/davinci-002-render-sha-mobile/.test(obj)) {
-  result = obj;
-} else {
-  result = obj.replace(/text-davinci-002-render-sha/g, "gpt-4-mobile");
-}
+var result = obj.replace(/^(?!.*davinci-002-render-sha-mobile).*text-davinci-002-render-sha.*$/g, function(match) {
+  return match.replace("text-davinci-002-render-sha", "gpt-4-mobile");
+});
         break;
     case findUrl(/accounts\/check\//):
         obj = JSON.parse($response.body);
