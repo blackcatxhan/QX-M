9c570d1f51ff723f399b006aa2c98fe7ff1b3603
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-26 16:37:09 +0700

    Update wutacamera.js

--- Scripts/Checking/wutacamera.js
+++ Scripts/Checking/wutacamera.js
@@ -2,15 +2,18 @@ let obj = JSON.parse($response.body);
 
 var currentTime = Math.floor(Date.now() / 1000);
 var oneHourAgo = currentTime - 3600;
-var oneMonthLater = currentTime + (30 * 24 * 60 * 60);
-var secondsInOneMonth = oneMonthLater - currentTime;
+// var oneMonthLater = currentTime + (30 * 24 * 60 * 60);
+var oneYearLater = currentTime + 31536000;
+var secondsInOneYear = oneYearLater - currentTime;
+
+// var secondsInOneMonth = oneMonthLater - currentTime;
 
 obj.data.vip_trial_start_time = oneHourAgo;
-obj.data.vip_show_expire_time = oneMonthLater;
+obj.data.vip_show_expire_time = oneYearLater;
 obj.data.vip_current_type = 2;
-obj.data.vip_expire_time = oneMonthLater;
+obj.data.vip_expire_time = oneYearLater;
 obj.data.vip_current_is_trial = false;
-obj.data.vip_trial_day = secondsInOneMonth;
+obj.data.vip_trial_day = secondsInOneYear;
 
 $done({
     body: JSON.stringify(obj)
