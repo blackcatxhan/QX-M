ab10826ee0b27d708adbc76415bd180fa16549e6
blackcatxhan <nguyendat.glhan@gmail.com>
2023-05-30 11:57:08 +0700

    Update musescore.js

--- Scripts/Checking/musescore.js
+++ Scripts/Checking/musescore.js
@@ -4,16 +4,6 @@ function findUrl(_reg) {
     }
 }
 
-function generateRandomNumber() {
-  var randomNum = Math.random();  // generates a random decimal between 0 and 1
-  var randomNumber = Math.floor(randomNum * 100000000);  // multiplies the decimal by 100000000 and rounds down to the nearest integer
-  return randomNumber;
-}
-
-var pad_id = generateRandomNumber();
-var payment_account_id = generateRandomNumber();
-var id = generateRandomNumber();
-
 let obj = JSON.parse($response.body);
 
 switch ($request.url) {
@@ -21,7 +11,7 @@ switch ($request.url) {
         obj = {
 		  "attributes" : {
 			"user_register_date" : 0,
-			"pad_id" : pad_id
+			"pad_id" : 36075622
 		  },
 		  "country" : "VN",
 		  "rt_analytics" : {
@@ -36,7 +26,7 @@ switch ($request.url) {
 			"retry_count" : 5
 		  },
 		  "time" : 1685420974000,
-		  "payment_account_id" : payment_account_id
+		  "payment_account_id" : 39557752
 		};
         break;
    case findUrl(/app\/link\/appsflyer/):
@@ -44,11 +34,11 @@ switch ($request.url) {
 		  "store" : "apple",
 		  "bundle_id" : "com.musescore.player",
 		  "appsflyer_id" : "1685369347652-5329518",
-		  "id" : id,
-		  "payment_account_device_id" : pad_id,
+		  "id" : 20211307,
+		  "payment_account_device_id" : 36075622,
 		  "advertising_id" : "",
 		  "date_created" : 1685420974,
-		  "payment_account_id" : payment_account_id
+		  "payment_account_id" : 39557752
 		};
         break;
     case findUrl(/purchase\/service\/access/):
