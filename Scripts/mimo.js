64611ac8570210140b8036bbcddf946c2ebe0d56
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-01 14:01:32 +0700

    Unlocked Mimo App

--- Scripts/mimo.js
+++ Scripts/mimo.js
@@ -1,34 +1,98 @@
-var obj = JSON.parse($response.body);
-
-obj= {
-  "source": "ios",
-  "status": "active",
-  "interval": "yearly",
-  "billingInfo": {
-    "currency": "USD",
-    "nextBillingPrice": 59.98999999999999
-  },
-  "subscriptions": [
-    {
-      "source": "ios",
-      "status": "active",
-      "interval": "yearly",
-      "billingInfo": {
-        "currency": "USD",
-        "nextBillingPrice": 59.98999999999999
-      },
-      "intervalCount": 1,
-      "activeUntil": "2099-10-10T08:04:21+00:00",
-      "createdAt": "2019-10-03T08:04:21+00:00",
-      "clientSecret": "",
-      "isActive": true
+function findUrl(_reg) {
+    if (_reg.test($request.url)) {
+        return $request.url;
     }
-  ],
-  "intervalCount": 1,
-  "activeUntil": "2099-10-10T08:04:21+00:00",
-  "createdAt": "2019-10-03T08:04:21+00:00",
-  "clientSecret": "",
-  "isActive": true
-};
+}
+let obj = JSON.parse($response.body);
 
-$done({body: JSON.stringify(obj)});
+const currentDate = new Date();
+const year = currentDate.getUTCFullYear();
+const month = String(currentDate.getUTCMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getUTCDate()).padStart(2, '0');
+const hours = String(currentDate.getUTCHours()).padStart(2, '0');
+const minutes = String(currentDate.getUTCMinutes()).padStart(2, '0');
+const seconds = String(currentDate.getUTCSeconds()).padStart(2, '0');
+const milliseconds = String(currentDate.getUTCMilliseconds()).padStart(3, '0');
+const formattedTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}+00:00`;
+
+switch ($request.url) {
+    case findUrl(/subscriptions\/receipt/):
+        obj = {
+		  "source" : "ios",
+		  "status" : "active",
+		  "interval" : "yearly",
+		  "trialEndAt" : "2099-07-07T07:07:07+00:00",
+		  "billingInfo" : {
+			"currency" : "VND",
+			"paymentPending" : false,
+			"nextBillingPrice" : 2299000
+		  },
+		  "intervalCount" : 1,
+		  "activeUntil" : "2099-07-07T07:07:07+00:00",
+		  "createdAt" : "2023-05-17T23:07:32+00:00",
+		  "type" : "pro",
+		  "isActive" : true
+		};
+        break;
+    case findUrl(/subscriptions/):
+        obj = {
+		  "source" : "ios",
+		  "status" : "active",
+		  "interval" : "yearly",
+		  "trialEndAt" : "2099-07-07T07:07:07+00:00",
+		  "subscriptions" : [
+			{
+			  "source" : "ios",
+			  "status" : "active",
+			  "interval" : "yearly",
+			  "trialEndAt" : "2099-07-07T07:07:07+00:00",
+			  "billingInfo" : {
+				"currency" : "USD",
+				"paymentPending" : false,
+				"nextBillingPrice" : 99.989999999999995
+			  },
+			  "intervalCount" : 1,
+			  "activeUntil" : "2099-07-07T07:07:07+00:00",
+			  "createdAt" : "2023-05-17T23:07:32+00:00",
+			  "type" : "pro",
+			  "isActive" : true
+			}
+		  ],
+		  "billingInfo" : {
+			"currency" : "USD",
+			"paymentPending" : false,
+			"nextBillingPrice" : 99.989999999999995
+		  },
+		  "intervalCount" : 1,
+		  "activeUntil" : "2099-07-07T07:07:07+00:00",
+		  "createdAt" : "2023-05-17T23:07:32+00:00",
+		  "type" : "pro",
+		  "isActive" : true
+		};
+        break;
+    case findUrl(/products\/user/):
+        const purchasedProductss = [];
+		obj.productsAvailableForPurchase.forEach(product => {
+			if (!(product.productType === "streak_challenge_7d")) {
+			  const newProduct = {
+				buyMode: "once",
+				id: (2548000 + Math.floor(Math.random() * 1000)),
+				boughtAt: formattedTime,
+				productType: product.productType,
+				price: product.price,
+				coinPrice: product.coinPrice
+			  };
+
+			  purchasedProductss.push(newProduct);
+			}
+		});
+		obj.purchasedProducts = purchasedProductss;
+        break;
+    case findUrl(/users\//):
+        obj.isPremium = true;
+        break;
+}
+
+$done({
+    body: JSON.stringify(obj)
+});
