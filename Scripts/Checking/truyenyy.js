c0c40a62b64ed783b9ccb4d4c65fbaeef005c772
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-02 14:42:03 +0700

    Update truyenyy.js

--- Scripts/Checking/truyenyy.js
+++ Scripts/Checking/truyenyy.js
@@ -1,9 +1,3 @@
-function findUrl(_reg) {
-    if (_reg.test($request.url)) {
-        return $request.url;
-    }
-}
-
 function replaceValueToAny(obj, key, value) {
   for (var prop in obj) {
     if (typeof obj[prop] === 'object') {
@@ -15,29 +9,30 @@ function replaceValueToAny(obj, key, value) {
 }
 
 let obj = JSON.parse($response.body);
+let url = $request.url;
 
-switch ($request.url) {
-    case findUrl(/auth\/verify-token/):
+switch (url) {
+    case url.includes("auth/verify-token"):
         obj.user.isPremium =  true;
         obj.user.isStaff =  true;
         obj.user.isSuperUser =  true;
         obj.user.premiumUntil =  "2099-07-07T07:07:07.832336+07:00";
         break;
-    case findUrl(/novel\/?novel_id/):
+    case url.includes("novel/?novel_id"):
         obj.novel.isVip = false;
         obj.novel.vipStartedAt = null;
         break;
-    case findUrl(/novel\/chapter\/?novel_id/):
+    case url.includes("novel/chapter/?novel_id"):
         replaceValueToAny(obj, 'vipStones', 0);
         replaceValueToAny(obj, 'isVip', false);
         replaceValueToAny(obj, 'isOwned', true);
         break;
-    case findUrl(/novel\/chapter\/raw-list\/?novel_id/):
+    case url.includes("novel/chapter/raw-list/?novel_id"):
         replaceValueToAny(obj, 'vipStones', 0);
         replaceValueToAny(obj, 'isVip', false);
         replaceValueToAny(obj, 'isOwned', true);
         break;
-    case findUrl(/novel\/chapter\/list\/?novel_id/):
+    case url.includes("novel/chapter/list/?novel_id"):
         replaceValueToAny(obj, 'vipStones', 0);
         replaceValueToAny(obj, 'isVip', false);
         replaceValueToAny(obj, 'isOwned', true);
