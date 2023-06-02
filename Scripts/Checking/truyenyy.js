a2993ed786a82ca2eea15ebc7ac950fd7dbdbe5a
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-02 15:27:10 +0700

    Updated

--- Scripts/Checking/truyenyy.js
+++ Scripts/Checking/truyenyy.js
@@ -4,14 +4,39 @@ function findUrl(_reg) {
     }
 }
 
+function replaceValueToAny(obj, key, value) {
+  for (var prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      replaceValueToAny(obj[prop], key, value);
+    } else if (prop === key) {
+      obj[prop] = value;
+    }
+  }
+}
+
 let obj = JSON.parse($response.body);
 
 switch ($request.url) {
     case findUrl(/auth\/verify-token/):
         obj.user.isPremium =  true;
-        obj.user.isStaff =  true;
         obj.user.isSuperUser =  true;
+        obj.user.goldTickets =  999999999;
+        obj.user.violetStones =  999999999;
+        obj.user.spiritStones =  999999999;
+        obj.user.magicStones =  999999999;
+        obj.user.bannedReasone =  "";
+        obj.user.banned =  false;
+        obj.user.bannedUntil =  null;
         obj.user.premiumUntil =  "2099-07-07T07:07:07.832336+07:00";
+        break;
+    case findUrl(/user\/purchase\/purchase-chapters-info\//):
+        obj.totalVipStones = 1;
+        break;
+    case findUrl(/user\/purchase\/purchase-chapters\//):
+        obj.requiredStones = 1;
+        obj.ok = true;
+        obj.notAfford = false;
+        obj.violetStones = 999999999;
         break;
 }
 
