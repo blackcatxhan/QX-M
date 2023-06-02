38d1cb65f1267f3673e09dd929050eca65f3c3d3
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-02 14:45:18 +0700

    Update truyenyy.js

--- Scripts/Checking/truyenyy.js
+++ Scripts/Checking/truyenyy.js
@@ -18,24 +18,14 @@ switch (url) {
         obj.user.isSuperUser =  true;
         obj.user.premiumUntil =  "2099-07-07T07:07:07.832336+07:00";
         break;
-    case url.includes("novel/?novel_id"):
+    case url.includes("novel/chapter/"):
+        replaceValueToAny(obj, 'vipStones', 0);
+        replaceValueToAny(obj, 'isVip', false);
+        replaceValueToAny(obj, 'isOwned', true);
+        break;
+    case url.includes("novel/"):
         obj.novel.isVip = false;
         obj.novel.vipStartedAt = null;
-        break;
-    case url.includes("novel/chapter/?novel_id"):
-        replaceValueToAny(obj, 'vipStones', 0);
-        replaceValueToAny(obj, 'isVip', false);
-        replaceValueToAny(obj, 'isOwned', true);
-        break;
-    case url.includes("novel/chapter/raw-list/?novel_id"):
-        replaceValueToAny(obj, 'vipStones', 0);
-        replaceValueToAny(obj, 'isVip', false);
-        replaceValueToAny(obj, 'isOwned', true);
-        break;
-    case url.includes("novel/chapter/list/?novel_id"):
-        replaceValueToAny(obj, 'vipStones', 0);
-        replaceValueToAny(obj, 'isVip', false);
-        replaceValueToAny(obj, 'isOwned', true);
         break;
 }
 
