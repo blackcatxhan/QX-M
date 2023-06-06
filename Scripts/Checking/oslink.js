0f2aacd2c6955da9686cca485f621dda06bd4e46
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-06 14:54:04 +0700

    Update oslink.js

--- Scripts/Checking/oslink.js
+++ Scripts/Checking/oslink.js
@@ -11,8 +11,9 @@ function replaceValueToAny(obj, key, value) {
 let obj = JSON.parse($response.body);
 
 replaceValueToAny(obj, 'cutofftime', 4087091227);
-replaceValueToAny(obj, 'minctime', 4087091227);
+replaceValueToAny(obj, 'viplist', "[{\"astrict\":0,\"cutofftime\":4087091227,\"minctime\":4087091227,\"viptype\":1}]");
 replaceValueToAny(obj, 'viptype', 1);
+replaceValueToAny(obj, 'astrict', 0);
 
 $done({
     body: JSON.stringify(obj)
