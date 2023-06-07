bb95b675cb6eddf353e4082e5af13e545ce745f9
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-07 11:33:48 +0700

    Update AI Mirror app

--- Scripts/Checking/aimirror.js
+++ Scripts/Checking/aimirror.js
@@ -1,9 +1,17 @@
+/*
+#!name=Unlock AI Mirror
+#!desc=AI Mirror
+[MITM]
+hostname= %APPEND% be.aimirror.fun
+[Script]
+AIMirror = type=http-response,pattern=^https:\/\/be\.aimirror\.fun\/(query_is_vip|config|draw(.*))\?(.*)$,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/blackcatxhan/QX-M/main/Scripts/Checking/aimirror.js,script-update-interval=-1
+*/
 function findUrl(_reg) {
   if (_reg.test($request.url)) {
     return $request.url;
   }
 }
-
+/*
 function replaceValueToAny(obj, key, value) {
   for (var prop in obj) {
     if (typeof obj[prop] === 'object') {
@@ -13,19 +21,21 @@ function replaceValueToAny(obj, key, value) {
     }
   }
 }
-
+*/
 let obj = JSON.parse($response.body);
 
 switch ($request.url){
 	case findUrl(/query_is_vip/):
 		obj =  true;
 		break;
+	/*
 	case findUrl(/config/):
 		replaceValueToAny(obj, 'is_free', true);
 		break;
-	case findUrl(/draw\/process/):
+	case findUrl(/draw/):
 		obj.is_vip = false;
 		break;
+	*/
 }
 
 $done({
