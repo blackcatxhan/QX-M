f9350eb9f4975179c4ce456c86f773a61aa53aba
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-07 09:54:25 +0700

    Update AI Mirror app

--- Scripts/Checking/aimirror.js
+++ Scripts/Checking/aimirror.js
@@ -1,3 +1,32 @@
+function findUrl(_reg) {
+  if (_reg.test($request.url)) {
+    return $request.url;
+  }
+}
+
+function replaceValueToAny(obj, key, value) {
+  for (var prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      replaceValueToAny(obj[prop], key, value);
+    } else if (prop === key) {
+      obj[prop] = value;
+    }
+  }
+}
+
+let obj;
+
+switch ($request.url){
+	case findUrl(/query_is_vip/):
+		obj =  true;
+		break;
+	case findUrl(/config/):
+		let resbody = JSON.parse($response.body);
+		replaceValueToAny(resbody, 'is_free', true);
+		obj = JSON.stringify(resbody);
+		break;
+}
+
 $done({
-    body: true
+	body: obj
 });
