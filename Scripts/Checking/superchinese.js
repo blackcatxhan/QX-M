a76c8d49a6f11c582840b537c5a612ee917eac22
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-08 09:52:59 +0700

    Update superchinese.js

--- Scripts/Checking/superchinese.js
+++ Scripts/Checking/superchinese.js
@@ -1,10 +1,10 @@
 let obj = JSON.parse($response.body);
 
-obj.vip_buy_url = null;
+// obj.vip_buy_url = null;
 obj.vip_lifetime = true;
 obj.vip_expired = 4087091227;
 obj.vip_label = null;
-obj.vip_icon = "@oss/icons/vip/ai1.png";
+// obj.vip_icon = "@oss/icons/vip/ai1.png";
 obj.vip = 1;
 
 
