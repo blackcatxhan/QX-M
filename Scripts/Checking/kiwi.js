8b2d168da033792f592bff20e4abeae2606000bb
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-09 17:37:29 +0700

    Updated

--- Scripts/Checking/kiwi.js
+++ Scripts/Checking/kiwi.js
@@ -1,12 +1,10 @@
 const headers = $request.headers;
 const selectedHeaders = {
   AppId: headers['AppId'],
-  'Content-Type': headers['Content-Type'],
   Authorization: headers['Authorization'],
   UserId: headers['UserId'],
   Token: headers['Token']
 };
-const headerString = JSON.stringify(selectedHeaders, null, 2);
-$notify("Selected Headers", "Test2", headerString);
-$clipboard.set(headerString);
+const headerString = JSON.stringify(selectedHeaders);
+$notify("Sao chép nó", "", headerString);
 $done({});
