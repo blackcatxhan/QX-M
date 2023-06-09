4d9de0e609c7fd478af11187f0c5e3b1e125bfcf
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-09 09:12:57 +0700

    Update recmycalls.js

--- Scripts/Checking/recmycalls.js
+++ Scripts/Checking/recmycalls.js
@@ -1,6 +1,8 @@
 let obj = JSON.parse($response.body);
 
-obj.data.apple_extra.original_application_version = "4.47.1";
+obj.data.apple_extra = {
+	"original_application_version" : "4.47.1"
+};
 
 $done({
     body: JSON.stringify(obj)
