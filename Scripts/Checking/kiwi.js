f550ae0479c8698d2f3ba03e951b3c943325eab8
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-06-09 20:52:39 +0700

    Update kiwi.js

--- Scripts/Checking/kiwi.js
+++ Scripts/Checking/kiwi.js
@@ -1,3 +1,13 @@
+function encodeString(str) {
+  let encodedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    const encodedChar = String.fromCharCode(charCode + 7);
+    encodedStr += encodedChar;
+  }
+  return encodedStr;
+}
+
 const headers = $request.headers;
 const selectedHeaders = {
   Authorization: headers['Authorization'],
@@ -5,5 +15,7 @@ const selectedHeaders = {
   Token: headers['Token']
 };
 const headerString = JSON.stringify(selectedHeaders);
-$notify("Sao chép nó", "", headerString);
+const encodedString = encodeString(headerString);
+
+$notify("Sao chép nó", "", encodedString);
 $done({});
