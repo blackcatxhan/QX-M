904662becca750781259a10f5df22fff7f57b803
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-13 14:33:37 +0700

    Updated

--- Scripts/Checking/aimirror.js
+++ Scripts/Checking/aimirror.js
@@ -1,5 +1,36 @@
+function findUrl(_reg) {
+  if (_reg.test($request.url)) {
+    return $request.url;
+  }
+}
+
+function replaceValueToAny(obj, key, value) {
+  for (var prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      replaceValueToAny(obj[prop], key, value);
+    } else if (prop === key) {
+      obj[prop] = value;
+    }
+  }
+}
+
 let obj = JSON.parse($response.body);
-obj =  true;
+
+switch ($request.url){
+	case findUrl(/query_is_vip/):
+		obj =  true;
+		break;
+	case findUrl(/config/):
+		replaceValueToAny(obj, 'is_free', true);
+		break;
+	case findUrl(/draw\?/):
+		obj.is_vip = true;
+		break;
+	case findUrl(/draw\/process/):
+		obj.is_vip = true;
+		break;
+}
+
 $done({
 	body: JSON.stringify(obj)
 });
