b594c2d56896702134521cbc841bb56019d08c34
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-06-25 12:15:45 +0700

    Updated app

--- Scripts/bendingspoonsapps.js
+++ Scripts/bendingspoonsapps.js
@@ -25,6 +25,30 @@ switch ($request.url) {
     case findUrl(/splice.oracle/):
         obj.me.non_consumables_ids =  ["com.path36.SpliceFree.lt_t73"];
         break;
+    case findUrl(/thirtydayfitness.oracle/):
+        obj.me.non_consumables_ids =  ["com.vigorapps.30DayFitness.1y_t130"];
+        break;
+    case findUrl(/sleep.oracle/):
+        obj.me.non_consumables_ids =  ["com.bendingspoonsapps.SleepHelp.1y_t100"];
+        break;
+    case findUrl(/focos.oracle/):
+        obj.me.non_consumables_ids =  ["com.focos.lifetime"]; //com.focos.1w_t8 or 1w_t8_1w
+        break;
+    case findUrl(/focoslive.oracle/):
+        obj.me.non_consumables_ids =  ["com.focoslive.lifetime"]; //com.focoslive.1w_t8 or 1w_t8_1w
+        break;
+    case findUrl(/yoga.oracle/):
+        obj.me.non_consumables_ids =  ["com.flyingnayeem.yoga.1w_t10"];
+        break;
+    case findUrl(/firstlight.oracle/):
+        obj.me.non_consumables_ids =  ["com.filmicpro.firstlight.1y_t100_bundle"];
+        break;
+    case findUrl(/alightmotion.oracle/):
+        obj.me.non_consumables_ids =  ["alightcreative.motion.1y_t60"];
+        break;
+    case findUrl(/doubletake.oracle/):
+        obj.me.non_consumables_ids =  ["com.filmicpro.doubletake.1y_t100_bundle"];
+        break;
 }
 
 $done({
