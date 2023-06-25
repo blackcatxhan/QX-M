674fa82d3bbfdac6acc6412ec0a4fd8ad5214bc1
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-06-25 12:38:30 +0700

    Unlocked remini, filmicpro, splice, thirtydayfitness, sleep, focos, focoslive, yoga, firstlight, doubletake apps

--- Scripts/bendingspoonsapps.js
+++ Scripts/bendingspoonsapps.js
@@ -26,11 +26,9 @@ switch ($request.url) {
         obj.me.non_consumables_ids =  ["com.path36.SpliceFree.lt_t73"];
         break;
     case findUrl(/thirtydayfitness.oracle/):
-        obj.me.non_consumables_ids =  ["com.vigorapps.30DayFitness.1y_t130"];
         obj.me.active_subscriptions_ids =  ["com.vigorapps.30DayFitness.1y_t130"];
         break;
     case findUrl(/sleep.oracle/):
-        obj.me.non_consumables_ids =  ["com.bendingspoonsapps.SleepHelp.1y_t100"];
         obj.me.active_subscriptions_ids =  ["com.bendingspoonsapps.SleepHelp.1y_t100"];
         break;
     case findUrl(/focos.oracle/):
@@ -40,25 +38,12 @@ switch ($request.url) {
         obj.me.non_consumables_ids =  ["com.focoslive.lifetime"];
         break;
     case findUrl(/yoga.oracle/):
-        obj.me.non_consumables_ids =  ["com.flyingnayeem.yoga.1w_t10"];
         obj.me.active_subscriptions_ids =  ["com.flyingnayeem.yoga.1w_t10"];
         break;
     case findUrl(/firstlight.oracle/):
-        obj.me.non_consumables_ids =  ["com.filmicpro.firstlight.1y_t130_bundle_creator"];
         obj.me.active_subscriptions_ids =  ["com.filmicpro.firstlight.1y_t130_bundle_creator"];
         break;
-    case findUrl(/alightmotion.oracle/):
-        obj.me.non_consumables_ids =  ["alightcreative.motion.1y_t60"];
-        obj.me.active_subscriptions_ids =  ["alightcreative.motion.1y_t60"];
-		obj.transactions = {
-			"300001475927460": {
-			  "error_code": null,
-			  "product_id": "alightcreative.motion.1y_t60"
-			}
-		};
-        break;
     case findUrl(/doubletake.oracle/):
-        obj.me.non_consumables_ids =  ["com.filmicpro.doubletake.1y_t130_bundle"];
         obj.me.active_subscriptions_ids =  ["com.filmicpro.doubletake.1y_t130_bundle"];
         break;
 }
