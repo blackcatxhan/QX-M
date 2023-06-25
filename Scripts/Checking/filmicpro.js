fe72c52aa3edf4afd1dd7bdf0b31070089b4d7e9
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-06-25 09:25:17 +0700

    Trying unlock Filmic Pro app

--- Scripts/Checking/filmicpro.js
+++ Scripts/Checking/filmicpro.js
@@ -1,5 +1,5 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.me.non_consumables_ids = ["com.cinegenix.filmic.pro.filmic_standard_t15"]
+obj.me.non_consumables_ids = ["com.cinegenix.filmic.pro.filmic_standard_t50"]
 body = JSON.stringify(obj);
 $done({body});
