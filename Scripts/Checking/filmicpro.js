26394d91d89a133dbf05fd0c01a5128523f36c6d
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-06-25 09:29:18 +0700

    Update filmicpro.js

--- Scripts/Checking/filmicpro.js
+++ Scripts/Checking/filmicpro.js
@@ -1,5 +1,5 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.me.non_consumables_ids = ["com.cinegenix.filmic.pro.filmic_standard_t50"]
+obj.me.non_consumables_ids = ["com.cinegenix.filmic.pro.filmic_standard_t15"]
 body = JSON.stringify(obj);
 $done({body});
