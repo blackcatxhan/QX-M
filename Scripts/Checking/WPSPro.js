ab29517ad602292c60213ce94b45e3e7602d8fe8
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-28 11:10:39 +0700

    Update WPS

--- Scripts/Checking/WPSPro.js
+++ Scripts/Checking/WPSPro.js
@@ -1,28 +1,67 @@
-function findUrl(_reg) {
-  if (_reg.test($request.url)) {
-    return $request.url;
-  }
-}
-
-let obj = JSON.parse($response.body);
-
-switch ($request.url) {
-    case findUrl(/mine\/vips/):
-        obj.vips = [{"name":"注册用户","has_ad":0,"memberid":10,"expire_time":1998890772,"enabled":null},{"name":"WPS Premium","has_ad":0,"memberid":210,"expire_time":4087091227,"enabled":null}];
-    /*
-	case findUrl(/order\/create/): // Restore purchase
-		After purchased: {"code":401,"message":"You have become a member, please be patient","data":null}
-		Before purchased: {"code":0,"message":"success","data":{"order_id":"ORDER_SG_CID8U1KF0TQFA1RLJB6G","order_db_id":0,"links":[{"href":"https://kpay-sg.wps.com/apple/api/%s/confirm","method":"POST"}],"third_product_id":"","apple_order_uuid":"bca79266-14bb-11ee-9db8-0a580af41848","client_secret":""}}
-        break;
-	*/
-    case findUrl(/permits\/check_snew/):
-		obj.data = [{"service_id":5,"expire_time":4087091227,"is_available":1,"fake_service_id":0,"permit_pack_id":0},{"service_id":211,"expire_time":4087091227,"is_available":1,"fake_service_id":0,"permit_pack_id":0},{"service_id":210,"expire_time":4087091227,"is_available":1,"fake_service_id":0,"permit_pack_id":0}];
-        break;
-    case findUrl(/api\/users/):
-		obj.vip = {"name":"WPS Premium","has_ad":0,"memberid":210,"expire_time":4087091227,"enabled":[]};
-        break;
-}
-
+var body = JSON.parse($response.body);
+var obj = {
+	exp: 0,
+	level: 3,
+	privilege: [{
+			spid: "data_recover",
+			times: 0,
+			expire_time: 1846256142
+		},
+		{
+			spid: "ocr",
+			times: 0,
+			expire_time: 1846256142
+		},
+		{
+			spid: "pdf2doc",
+			times: 0,
+			expire_time: 1846256142
+		},
+		{
+			spid: "pdf_merge",
+			times: 0,
+			expire_time: 1846256142
+		},
+		{
+			spid: "pdf_sign",
+			times: 0,
+			expire_time: 1846256142
+		},
+		{
+			spid: "pdf_split",
+			times: 0,
+			expire_time: 1846256142
+		}
+	],
+	result: "ok",
+	total_buy: 0,
+	total_cost: -30,
+	userid: body.userid,
+	vip: {
+		name: "超级会员",
+		has_ad: 0,
+		memberid: 40,
+		expire_time: 1846256142,
+		enabled: [{
+				memberid: 40,
+				name: "超级会员",
+				expire_time: 1846256142
+			},
+			{
+				memberid: 20,
+				name: "WPS会员",
+				expire_time: 1846256142
+			},
+			{
+				memberid: 12,
+				name: "稻壳会员",
+				expire_time: 1846256142
+			}
+		]
+	},
+	wealth: 0,
+	expire_time: 1846256142
+};
 $done({
 	body: JSON.stringify(obj)
 });
