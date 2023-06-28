000b7b5c88b72196ecf4885c4fedb6ec49c26cf2
blackcatxhan <nguyendat.glhan@gmail.com>
2023-06-28 15:34:21 +0700

    Updated Polarr

--- Scripts/Polarr.js
+++ Scripts/Polarr.js
@@ -1,17 +1,4 @@
-const sta = typeof $task !== "undefined" ? "HTTP/1.1 200 OK" : 200;
-const res = $response;
-const req = $request;
-
-if (res.body && req.method == "PUT") {
-  $done({
-    body: JSON.stringify({
-      "app": "PPE",
-      "isUnlimited": true,
-      "membershipExpiryDate": "2099-07-07T07:07:07.000Z"
-    }),
-    status: sta,
-    headers: res.headers
-  });
-} else {
-  $done({});
-}
+let obj = JSON.parse($response.body);
+obj.isUnlimited = true,
+obj.membershipExpiryDate = "2099-07-07T07:07:07.000Z",
+$done({body: JSON.stringify(obj)});
