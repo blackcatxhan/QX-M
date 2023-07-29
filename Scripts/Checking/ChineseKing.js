050504a1123753c788d3254f23ac16f8b84431b4
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2023-07-29 21:13:11 +0700

    Unlocked Chinese King app

---
+++ Scripts/Checking/ChineseKing.js
@@ -0,0 +1,18 @@
+function replaceValueToAny(obj, key, value) {
+  for (var prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      replaceValueToAny(obj[prop], key, value);
+    } else if (prop === key) {
+      obj[prop] = value;
+    }
+  }
+}
+
+let obj = JSON.parse($response.body);
+
+replaceValueToAny(obj, 'bought', 'true');
+replaceValueToAny(obj, 'courses', '[{"id":1,"title:"LK-Team","price":0}]');
+
+$done({
+	body: JSON.stringify(obj)
+});
