7d9202f603d3707a835e0db0b24117ff57a52c4c
blackcatxhan <nguyendat.glhan@gmail.com>
2023-11-29 16:11:13 +0700

    Fixed Nicegram

--- Scripts/nicegramv6.js
+++ Scripts/nicegramv6.js
@@ -1,5 +1,21 @@
-let obj = JSON.parse(body);
-obj.data.user.subscription = true;
-obj.data.user.lifetime_subscription = true;
-body = JSON.stringify(obj); 
-$done({body});
+function findUrl(_reg) {
+    if (_reg.test($request.url)) {
+        return $request.url;
+    }
+}
+
+let obj = JSON.parse($response.body);
+
+switch ($request.url) {
+    case findUrl(/my.nicegram.app\/api\/v4\/settings/):
+        obj.premium = true;
+        break;
+    case findUrl(/nicegram.cloud\/api\/v6\/(user\/info|telegram\/auth)/):
+        obj.data.user.subscription = true;
+		obj.data.user.lifetime_subscription = true;
+        break;
+}
+
+$done({
+    body: JSON.stringify(obj)
+});
