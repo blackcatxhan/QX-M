ea58d57d5b16634577477b18c13c582d2a84643a
blackcatxhan <nguyendat.glhan@gmail.com>
2024-03-25 10:50:08 +0700

    Update truecaller.js

--- Scripts/truecaller.js
+++ Scripts/truecaller.js
@@ -4,46 +4,84 @@ function findUrl(_reg) {
   }
 }
 const features = [
-  { id: "spam_blocking", rank: -2147483648, status: "Included" },
-  { id: "caller_id", rank: -2147483648, status: "Included" },
-  { id: "no_ads", rank: 1, status: "Included" },
-  { id: "call_assistant", rank: 2, status: "Included" },
-  { id: "extended_spam_blocking", rank: 3, status: "Included" },
-  { id: "who_viewed_my_profile", rank: 4, status: "Included" },
-  { id: "incognito_mode", rank: 6, status: "Included" },
-  { id: "premium_badge", rank: 10, status: "Included" },
-  { id: "premium_support", rank: 11, status: "Included" },
-  { id: "gold_caller_id", rank: 13, status: "Included" }
-];
-const features_sub = [
-  { id: "spam_blocking", rank: -2147483648, status: "Included", isFree: true },
-  { id: "caller_id", rank: -2147483648, status: "Included", isFree: true },
-  { id: "siri_search", rank: 1, status: "Included", isFree: false },
-  { id: "no_ads", rank: 2, status: "Included", isFree: false },
-  { id: "extended_spam_blocking", rank: 3, status: "Included", isFree: false },
-  { id: "call_assistant", rank: 4, status: "Excluded", isFree: false },
-  { id: "assistant_voicemail", rank: 5, status: "Excluded", isFree: false },
-  { id: "assistant_custom_greeting", rank: 6, status: "Excluded", isFree: false },
-  { id: "ct_call_recording", rank: 7, status: "Excluded", isFree: false },
-  { id: "who_viewed_my_profile", rank: 9, status: "Included", isFree: false },
-  { id: "incognito_mode", rank: 11, status: "Included", isFree: false },
-  { id: "premium_badge", rank: 15, status: "Included", isFree: false },
-  { id: "premium_support", rank: 16, status: "Included", isFree: false },
-  { id: "live_chat_support", rank: 17, status: "Included", isFree: false },
-  { id: "family_sharing", rank: 18, status: "Excluded", isFree: false },
-  { id: "gold_caller_id", rank: 19, status: "Included", isFree: false }
+	{
+	  "status" : "Included",
+	  "id" : "siri_search",
+	  "isFree" : false,
+	  "rank" : 1
+	},
+	{
+	  "status" : "Included",
+	  "id" : "no_ads",
+	  "isFree" : false,
+	  "rank" : 2
+	},
+	{
+	  "status" : "Included",
+	  "id" : "extended_spam_blocking",
+	  "isFree" : false,
+	  "rank" : 3
+	},
+	{
+	  "status" : "Included",
+	  "id" : "ct_call_recording",
+	  "isFree" : false,
+	  "rank" : 7
+	},
+	{
+	  "status" : "Included",
+	  "id" : "who_viewed_my_profile",
+	  "isFree" : false,
+	  "rank" : 9
+	},
+	{
+	  "status" : "Included",
+	  "id" : "incognito_mode",
+	  "isFree" : false,
+	  "rank" : 11
+	},
+	{
+	  "status" : "Included",
+	  "id" : "premium_badge",
+	  "isFree" : false,
+	  "rank" : 15
+	},
+	{
+	  "status" : "Included",
+	  "id" : "premium_support",
+	  "isFree" : false,
+	  "rank" : 16
+	},
+	{
+	  "status" : "Included",
+	  "id" : "live_chat_support",
+	  "isFree" : false,
+	  "rank" : 17
+	},
+	{
+	  "status" : "Included",
+	  "id" : "premium_app_icon",
+	  "isFree" : false,
+	  "rank" : 19
+	},
+	{
+	  "status" : "Included",
+	  "id" : "gold_caller_id",
+	  "isFree" : false,
+	  "rank" : 20
+	}
 ];
 var obj;
 switch ($request.url) {
   case findUrl(/subscriptions\/status/):
       obj = {
         expire: "2099-07-07T11:20:25Z",
+		subscriptionStatus: "SUBSCRIBED",
         start: "2022-09-03T11:20:25Z",
+		isGracePeriodExpired: false,
+		isExpired: false,
+		inAppPurchaseAllowed: true,
         paymentProvider: "Apple",
-        isExpired: false,
-        isGracePeriodExpired: false,
-        subscriptionStatus: "SUBSCRIBED",
-        inAppPurchaseAllowed: true,
         product: {
           id: "renewable.premiumgold.annual",
           sku: "renewable.premiumgold.annual",
@@ -51,7 +89,7 @@ switch ($request.url) {
           productType: "GoldYearly",
           isFreeTrial: false
         },
-        tier: { id: "gold", feature: features_sub }
+        tier: { id: "gold", feature: features }
       }
     break;
   case findUrl(/products\/apple/):
@@ -62,8 +100,8 @@ switch ($request.url) {
       "product": [
         {
           "productType": "GoldYearly",
-          "id": "renewable.gold.annual",
-          "sku": "renewable.gold.annual",
+          "id": "renewable.premiumgold.annual",
+          "sku": "renewable.premiumgold.annual",
           "contentType": "subscription",
           "rank": 6,
           "paymentProvider": "Apple",
