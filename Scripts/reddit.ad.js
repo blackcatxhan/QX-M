addde439b2209b208cf92889f1ebc958fbb96961
blackcatxhan <nguyendat.glhan@gmail.com>
2024-04-02 11:11:40 +0700

    Update reddit.ad.js

--- Scripts/reddit.ad.js
+++ Scripts/reddit.ad.js
@@ -3,8 +3,8 @@ if ($response.body.includes("adEvents")) {
   if (body.data.subredditInfoByName) {
     body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter((n) => !!!n.node.adEvents);
     $done({ body: JSON.stringify(body) });
-  } else if (body.data.home) {
-    body.data.home.elements.edges = body.data.home.elements.edges.filter((n) => !!!n.node.adEvents);
+  } else if (body.data.homeV3) {
+    body.data.homeV3.elements.edges = body.data.homeV3.elements.edges.filter((n) => !!!n.node.adEvents);
     $done({ body: JSON.stringify(body) });
   } else {
     $done({});
