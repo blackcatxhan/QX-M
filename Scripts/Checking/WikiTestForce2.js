8650ce55a70b42dc17a29800f1dfd7ae2426c5c0
blackcatxhan <nguyendat.glhan@gmail.com>
2024-10-15 10:39:02 +0700

    Removed

--- Scripts/Checking/WikiTestForce2.js
+++
@@ -1,9 +0,0 @@
-let obj = JSON.parse($response.body);
-obj = {
-	"requestedString": "newreplacetext",
-	"currentString": "gptpara",
-	"updatedString": "newreplacetext"
-};
-$done({
-	body: JSON.stringify(obj)
-});
