bbfe8344c2581715207e6eba3a9fd3463560ebd6
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-09-12 13:32:22 +0700

    Remove

--- Scripts/Checking/Inject.js
+++
@@ -1,9 +0,0 @@
-let obj = {
-  "platform": "ios",
-  "productId": "com.rostamimagic.inject",
-  "receipt": "MA==5122",
-  "email": "nguyendat.glhan@gmail.com",
-  "password": null
-}
-
-$done({response: {body: JSON.stringify(obj)}});
