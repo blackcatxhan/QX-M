a0afc3fc3322624c5d1e575c0e152d2525c8c059
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-09-17 17:35:16 +0700

    Update Inject.js

--- Scripts/Checking/Inject.js
+++ Scripts/Checking/Inject.js
@@ -1,25 +1,15 @@
-// Inject.js
-console.log(`Request method: ${$request.method}`);
-console.log(`Request URL: ${$request.url}`);
-
 if ($request.method !== "POST") {
-    console.log(`Skipping ${$request.method} request, only processing POST`);
     $done({});
     return;
 }
 
-console.log("Processing POST request...");
-console.log(`Original body: ${$request.body}`);
-
 const modifiedBody = JSON.stringify({
     "platform": "ios",
     "productId": "com.rostamimagic.inject", 
-    "receipt": "77",
+    "receipt": "MA==",
     "email": null,
     "password": null
 });
-
-console.log(`Modified body: ${modifiedBody}`);
 
 $done({
     body: modifiedBody
