1de7fbd2b21787c8455bfb538109898cd24c2717
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-09-17 16:46:44 +0700

    Update
    - Move OneAHead to Checking
    - Change URL for iPA Install
    - Trying Unlock Inject again

---
+++ Scripts/Checking/Inject.js
@@ -0,0 +1,9 @@
+const obj = {
+  "platform": "ios",
+  "productId": "com.rostamimagic.inject",
+  "receipt": "77",
+  "email": "mqluus@gmail.com",
+  "password": "mqluus"
+};
+
+$done({response: {body: JSON.stringify(obj)}})
