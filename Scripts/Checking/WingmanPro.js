654b7e6b39be32a7e4cf7e9a3487315c68b06933
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-10-21 17:28:41 +0700

    Update

--- Scripts/Checking/WingmanPro.js
+++ Scripts/Checking/WingmanPro.js
@@ -1,17 +1,47 @@
+function findUrl(_reg) {
+  if (_reg.test($request.url)) {
+    return $request.url;
+  }
+}
 let obj = JSON.parse($response.body);
 
-obj.data.data = [
-	{
-		"id": 1024,
-		"firstname": "Mr",
-		"lastname": "Mqluus",
-		"email": "mqluus+wingman@gmail.com",
-		"uuid": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
-		"app": "12faf4fc-127a-4fe6-9ebf-513289658b22",
-		"unlimitedAccess": true,
-		"connectID": ""
-	}
-];
+switch ($request.url){
+	case findUrl(/getUser/):
+		obj.data.data = [
+			{
+				"id": 1024,
+				"firstname": "Mr",
+				"lastname": "Mqluus",
+				"email": "mqluus+wingman@gmail.com",
+				"uuid": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
+				"app": "12faf4fc-127a-4fe6-9ebf-513289658b22",
+				"unlimitedAccess": true,
+				"connectID": ""
+			}
+		];
+		break;
+	case findUrl(/verify/):
+		obj = {
+			"type": "success",
+			"errors": [],
+			"licenses": [
+				{
+					"uuid": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
+					"code": "QLZQ-XXOW-HYCP-HJP2",
+					"appID": "12faf4fc-127a-4fe6-9ebf-513289658b22",
+					"userID": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
+					"deviceID": "GHQICX",
+					"deviceName": "iPhone 13",
+					"disabled": null,
+					"createdBy": null,
+					"lastUsed": "2025-07-14T04:54:28.027223",
+					"createdAt": "2025-07-14T04:54:28.027223",
+					"chargeID": "ch_3RkePEE8fzS13Pu10JwfWGQT"
+				}
+			]
+		}
+		break;
+}
 
 $done({
 	body: JSON.stringify(obj)
