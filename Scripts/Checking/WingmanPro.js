1f4d42e920b384f05b68d40e418bc6650418159b
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-10-21 17:13:46 +0700

    update

--- Scripts/Checking/WingmanPro.js
+++ Scripts/Checking/WingmanPro.js
@@ -1,1 +1,18 @@
-$done({ body: "true" });
+let obj = JSON.parse($response.body);
+
+obj.data.data = [
+	{
+		"id": 1024,
+		"firstname": "Mr",
+		"lastname": "Mqluus",
+		"email": "mqluus+wingman@gmail.com",
+		"uuid": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
+		"app": "12faf4fc-127a-4fe6-9ebf-513289658b22",
+		"unlimitedAccess": true,
+		"connectID": ""
+	}
+];
+
+$done({
+	body: JSON.stringify(obj)
+});
