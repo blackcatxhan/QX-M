492fe9f92d7852357befd95a7002a781d607d75e
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-10-22 08:38:27 +0700

    update

--- Scripts/Checking/WingmanPro2.js
+++ Scripts/Checking/WingmanPro2.js
@@ -1,23 +1,15 @@
-let obj = {
-	"type": "success",
-	"errors": [],
-	"licenses": [
-		{
-			"uuid": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
-			"code": "QLZQ-XXOW-HYCP-HJP2",
-			"appID": "12faf4fc-127a-4fe6-9ebf-513289658b22",
-			"userID": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
-			"deviceID": "GHQICX",
-			"deviceName": "iPhone 13",
-			"disabled": null,
-			"createdBy": null,
-			"lastUsed": "2025-07-14T04:54:28.027223",
-			"createdAt": "2025-07-14T04:54:28.027223",
-			"chargeID": "ch_3RkePEE8fzS13Pu10JwfWGQT"
-		}
-	]
+if ($request.method !== "POST") {
+    $done({});
+    return;
 }
 
+const modifiedBody = JSON.stringify({
+    "deviceName": "iPhone18,1",
+    "appID": "12faf4fc-127a-4fe6-9ebf-513289658b22",
+    "deviceID": "EOURLX",
+    "user": "6d737aac-936f-4f7b-b659-8f199c175257"
+});
+
 $done({
-	body: JSON.stringify(obj)
+    body: modifiedBody
 });
---
+++ Scripts/Checking/WingmanPro3.js
@@ -0,0 +1,23 @@
+let obj = {
+	"type": "success",
+	"errors": [],
+	"licenses": [
+		{
+			"uuid": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
+			"code": "QLZQ-XXOW-HYCP-HJP2",
+			"appID": "12faf4fc-127a-4fe6-9ebf-513289658b22",
+			"userID": "a35eb98f-f884-46b3-8b77-0933298fa0fb",
+			"deviceID": "GHQICX",
+			"deviceName": "iPhone 13",
+			"disabled": null,
+			"createdBy": null,
+			"lastUsed": "2025-07-14T04:54:28.027223",
+			"createdAt": "2025-07-14T04:54:28.027223",
+			"chargeID": "ch_3RkePEE8fzS13Pu10JwfWGQT"
+		}
+	]
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
