1cab42bc83f07d9ba3afb9f486d145f892176343
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-10-24 09:30:02 +0700

    Update

---
+++ Scripts/Checking/WingmanPro4.js
@@ -0,0 +1,17 @@
+function replaceValueToAny(obj, key, value) {
+  for (var prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      replaceValueToAny(obj[prop], key, value);
+    } else if (prop === key) {
+      obj[prop] = value;
+    }
+  }
+}
+
+let obj = JSON.parse($response.body);
+
+replaceValueToAny(obj, 'price', 0);
+
+$done({
+	body: JSON.stringify(obj)
+});
