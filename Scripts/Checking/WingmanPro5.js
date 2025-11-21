4bfa3b4daa59dca2714153b21270d48c25252326
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-11-21 13:56:37 +0700

    Unlock Add-ons

---
+++ Scripts/Checking/WingmanPro5.js
@@ -0,0 +1,12 @@
+let obj = {
+    "type": "success",
+    "has_access": false,
+    "addon_found": true,
+    "addon_id": "ed063df5-8897-40c2-bf0c-6abd0a1baf5c",
+    "addon_slug": "wingman-sites",
+    "addon_name": "Wingman Sites"
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
