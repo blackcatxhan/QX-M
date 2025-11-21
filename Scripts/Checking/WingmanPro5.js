d2aa7d2cd94e2b7c85920ca5aff03068e56bac15
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2025-11-21 13:57:57 +0700

    Update WingmanPro5.js

--- Scripts/Checking/WingmanPro5.js
+++ Scripts/Checking/WingmanPro5.js
@@ -1,6 +1,6 @@
 let obj = {
     "type": "success",
-    "has_access": false,
+    "has_access": true,
     "addon_found": true,
     "addon_id": "ed063df5-8897-40c2-bf0c-6abd0a1baf5c",
     "addon_slug": "wingman-sites",
