2ab8d9d6823d755519c1f97f7f5bcc559a29682a
blackcatxhan <nguyendat.glhan@gmail.com>
2025-12-23 12:55:30 +0700

    Update and remove

--- Scripts/Checking/MyPasSwordX.js
+++ Scripts/Checking/MyPasSwordX.js
@@ -1,7 +1,24 @@
-let obj = {
-  message: "License key is valid. You can continue registration.",
-  licenseStatus: "active"
-};
+function findUrl(_reg) {
+  if (_reg.test($request.url)) {
+    return $request.url;
+  }
+}
+
+let obj = JSON.parse($response.body);
+
+switch ($request.url){
+	case findUrl(/auth\/start-registration/):
+		obj = {
+		  message: "License key is valid. You can continue registration.",
+		  licenseStatus: "active"
+		};
+		break;
+	case findUrl(/auth\/login/):
+		obj = {
+		  message: "Login is success."
+		};
+		break;
+}
 
 $done({
   status: "HTTP/1.1 200 OK",
