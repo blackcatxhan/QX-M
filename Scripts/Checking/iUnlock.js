3a9bd268513a1e07d37d9aa21ee3a2d363d4f3d9
blackcatxhan <nguyendat.glhan@gmail.com>
2026-01-05 13:33:48 +0700

    Update iUnlock.js

--- Scripts/Checking/iUnlock.js
+++ Scripts/Checking/iUnlock.js
@@ -3,11 +3,19 @@ let obj = JSON.parse($response.body);
 
 if (/login\.php/.test(url)) {
 	obj = {
-		"status": "success"
+	  "status" : "success",
+	  "password" : "123456789",
+	  "deviceId" : "IPhoneApp ver-3 (3)",
+	  "id" : 2579,
+	  "code" : "LOPRO_7541596852",
+	  "email" : "johnquanta@mailnesia.com",
+	  "name" : "John Quanta"
 	};
 } else if (/register\.php/.test(url)) {
 	obj = {
-		"status": "success"
+	  "status" : "success",
+	  "username" : "johnquanta@mailnesia.com",
+	  "password" : "123456789"
 	};
 }
 
