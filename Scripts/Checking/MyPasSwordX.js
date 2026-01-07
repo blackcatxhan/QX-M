49e19873832504c3e25974caf8c2de14a7809347
blackcatxhan <nguyendat.glhan@gmail.com>
2026-01-07 16:58:34 +0700

    Update MyPasSwordX.js

--- Scripts/Checking/MyPasSwordX.js
+++ Scripts/Checking/MyPasSwordX.js
@@ -1,26 +1,29 @@
-function findUrl(_reg) {
-  if (_reg.test($request.url)) {
-    return $request.url;
-  }
+function matchUrl(reg) {
+  return reg.test($request.url);
 }
 
 let obj = JSON.parse($response.body);
+let new_obj;
 
-switch ($request.url){
-	case findUrl(/auth\/start-registration/):
-		let new_obj = {
-		  message: "License key is valid. You can continue registration.",
-		  licenseStatus: "active"
-		};
-		break;
-	case findUrl(/auth\/login/):
-		let new_obj = {
-		  message: "Login is success."
-		  email: obj.email,
-		  password: obj.password,
-		  deviceId: obj.deviceId
-		};
-		break;
+switch (true) {
+  case matchUrl(/auth\/start-registration/):
+    new_obj = {
+      message: "License key is valid. You can continue registration.",
+      licenseStatus: "active"
+    };
+    break;
+
+  case matchUrl(/auth\/login/):
+    new_obj = {
+      message: "Login is success.",
+      email: obj.email,
+      password: obj.password,
+      deviceId: obj.deviceId
+    };
+    break;
+
+  default:
+    new_obj = obj;
 }
 
 $done({
