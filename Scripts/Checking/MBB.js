23009d22a3f930120330cbfdaefc5f124628e89c
blackcatxhan <87691886+blackcatxhan@users.noreply.github.com>
2026-01-18 15:59:44 +0700

    Update case for MBB

--- Scripts/Checking/MBB.js
+++ Scripts/Checking/MBB.js
@@ -1,6 +1,6 @@
 function replaceValueToAny(obj, key, value) {
   for (var prop in obj) {
-    if (typeof obj[prop] === 'object') {
+    if (typeof obj[prop] === 'object' && obj[prop] !== null) {
       replaceValueToAny(obj[prop], key, value);
     } else if (prop === key) {
       obj[prop] = value;
@@ -8,12 +8,26 @@ function replaceValueToAny(obj, key, value) {
   }
 }
 
+function findUrl(reg) {
+  return reg.test($request.url);
+}
+
 let obj = JSON.parse($response.body);
 
-replaceValueToAny(obj, 'success', true);
-replaceValueToAny(obj, 'message', "OK");
-replaceValueToAny(obj, 'validUntil', "2099-12-30T08:00:00Z");
+// For: rpc/redeem_license_on_device
+if (findUrl(/rpc\/redeem_license_on_device/)) {
+  replaceValueToAny(obj, 'success', true);
+  replaceValueToAny(obj, 'message', "OK");
+  replaceValueToAny(obj, 'validUntil', "2099-12-30T08:00:00Z");
+}
+
+// For: licenses?user_id
+if (findUrl(/licenses\?user_id=/)) {
+  replaceValueToAny(obj, 'paid', true);
+  replaceValueToAny(obj, 'is_active', true);
+  replaceValueToAny(obj, 'trial_expires_at', "2099-12-30T08:00:00.003459+00:00");
+}
 
 $done({
-	body: JSON.stringify(obj)
+  body: JSON.stringify(obj)
 });
