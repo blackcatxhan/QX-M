70cafcfca4374b596bde04ba1dbfbfa5b0237247
blackcatxhan <nguyendat.glhan@gmail.com>
2026-08-31 21:23:32 +0700

    Extend expiry to one year and simplify logic
    Change expiration handling from 30 days to 1 year. Replace ONE_MONTH/ addOneMonth helper with a single ONE_YEAR constant and compute newExpireDate once. Assign newExpireDate to user.expireDate in /GetProfile and to obj.data.expireDate in /GetUserExpireDate, while keeping userId extraction intact. Simplifies and consolidates expiry logic.

--- Scripts/Checking/Vietmap.js
+++ Scripts/Checking/Vietmap.js
@@ -1,23 +1,16 @@
 var obj = JSON.parse($response.body);
-
 var url = $request.url;
 
-// One month in seconds (30 days)
-var ONE_MONTH = 30 * 24 * 60 * 60;
+// 1 year in seconds
+var ONE_YEAR = 365 * 24 * 60 * 60;
 
-// Function to add 1 month to expireDate
-function addOneMonth(expireDate) {
-  if (expireDate && expireDate > 0) {
-    return expireDate + ONE_MONTH;
-  }
-  // If expireDate is 0 or null, set to 1 month from now
-  return Math.floor(Date.now() / 1000) + ONE_MONTH;
-}
+// Get current time + 1 year
+var newExpireDate = Math.floor(Date.now() / 1000) + ONE_YEAR;
 
 // GetProfile endpoint
 if (url.indexOf("/GetProfile") !== -1) {
   if (obj.data && obj.data.user) {
-    obj.data.user.expireDate = addOneMonth(obj.data.user.expireDate);
+    obj.data.user.expireDate = newExpireDate;
   }
 }
 
@@ -32,14 +25,12 @@ if (url.indexOf("/GetFeaturePacks") !== -1) {
 
 // GetUserExpireDate endpoint
 if (url.indexOf("/GetUserExpireDate") !== -1) {
-  // Extract userId from URL parameter
   var urlMatch = url.match(/userId=(\d+)/);
   if (urlMatch && obj.data) {
     obj.data.userId = parseInt(urlMatch[1]);
   }
-  // Set new expireDate (current + 1 month)
   if (obj.data) {
-    obj.data.expireDate = addOneMonth(obj.data.expireDate);
+    obj.data.expireDate = newExpireDate;
   }
 }
 
